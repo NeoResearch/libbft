be38b859953859ea29981f31ab109b4394763b89
Igor Machado <igor.machado@gmail.com>
2019-07-24 17:39:20 -0300

    starting to model basic dbft

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -45,13 +45,49 @@ simpleExample()
    machine.run(initial);
 }
 
+struct dBFTData
+{
+   // view number
+   int v;
+   // blockchain height
+   int H;
+};
+
+void
+dbft()
+{
+   State<dBFTData>* initial = new State<dBFTData>(false, "Initial");
+   State<dBFTData>* final = new State<dBFTData>(true, "Final");
+
+   Transition<dBFTData>* alwaysTrue = new Transition<dBFTData>(final, "always true");
+   alwaysTrue->timedFunction = [](Timer& t) -> bool { return true; };
+
+   Transition<dBFTData>* after1sec = new Transition<dBFTData>(final, "after1sec");
+   after1sec->timedFunction = [](Timer& t) -> bool { return t.elapsedTime() >= 1.0; };
+
+   //initial->transitions.push_back(alwaysTrue);
+   initial->transitions.push_back(after1sec);
+
+   //cout << "initial state: " << initial->toString() << endl;
+   //cout << "final state: " << final->toString() << endl;
+
+   SingleTimerStateMachine<dBFTData> machine(new Timer("C"));
+
+   machine.registerState(initial);
+   machine.registerState(final);
+
+   cout << "Machine => " << machine.toString() << endl;
+
+   machine.run(initial);
+}
+
 int
 main()
 {
    cout << "begin test state machines!" << endl;
 
    simpleExample();
-   //simpleExample();
+   dbft();
 
    cout << "finished successfully!" << endl;
 
