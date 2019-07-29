d33837d78a8ae1d23a74cc4e6f10d5552ba73b82
Igor Machado <igor.machado@gmail.com>
2019-07-28 21:58:46 -0300

    todo fix clock

--- spec/cpp/src/timing/Clock.hpp
+++ spec/cpp/src/timing/Clock.hpp
@@ -3,11 +3,13 @@
 #define LIBBFT_SRC_CPP_CLOCK_HPP
 
 // system includes
+#include <chrono>
 #include <sstream>
 
 // standard Timer for a TSM
 
-using namespace std; // TODO: remove
+using namespace std;         // TODO: remove
+using namespace std::chrono; // TODO: remove
 
 namespace libbft {
 
@@ -29,8 +31,15 @@ public:
    // get time in seconds
    virtual double getTime()
    {
-      // terrible, only seconds! should be micro at least...
-      return time(NULL);
+      // using chrono
+      system_clock::time_point now = system_clock::now();
+      system_clock::duration tp = now.time_since_epoch(); // which epoch.. unix? // TODO: check
+
+      tp -= duration_cast<seconds>(tp);
+
+      auto f_secs = std::chrono::duration_cast<std::chrono::duration<float>>(tp);
+      // todo: fix this clock to return seconds in double/float format
+      return f_secs.count();
    }
 
    string toString() const
