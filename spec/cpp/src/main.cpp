c4ab1fd0ca6c6a8d5fe48e8a0884d59ded0a6b7c
Igor Machado <igor.machado@gmail.com>
2019-08-20 12:07:45 -0300

    removed create_dBFT method

--- spec/cpp/src/main.cpp
+++ spec/cpp/src/main.cpp
@@ -54,102 +54,16 @@ simpleExample()
    string graphviz = machine.toString("graphviz");
    cout << graphviz << endl;
 
-   // set set machine watchdog - 5 seconds - Default disabled 
+   // set set machine watchdog - 5 seconds - Default disabled
    machine.setWatchdog(5);
 
    machine.run(); // initial state is given by default first
-   
+
    FILE* fgraph = fopen("fgraph_STSM.dot", "w");
    fprintf(fgraph, "%s\n", graphviz.c_str());
    fclose(fgraph);
    cout << "Generating image 'fgraph_STSM.png'" << endl;
    system("dot -Tpng fgraph_STSM.dot -o fgraph_STSM.png");
-}
-
-void
-dbft_test_backup_multi()
-{
-   auto machine0 = create_dBFTMachine(0);
-
-   // insert pre-initial state
-   auto preinitial = new State<MultiContext<dBFT2Context>>(false, "PreInitial");
-
-   machine0->states.insert(machine0->states.begin() + 0, preinitial);
-
-   cout << "Machine => " << machine0->toString() << endl;
-
-   // v = 0, H = 1501, T = 3 (secs), R = 1 (one node network)
-   dBFT2Context data(0, 1501, 3, 2); // 1501 -> backup (R=2)
-
-   MultiContext<dBFT2Context> ctx;
-   ctx.vm.push_back(MachineContext<dBFT2Context>(&data, machine0));
-
-   ReplicatedSTSM<dBFT2Context> multiMachine;
-   multiMachine.registerMachine(machine0);
-
-   // global transition scheduled to start machine 0 ("OnStart") after 1 second
-   /*
-   multiMachine.scheduleGlobalTransition(
-     (new Timer())->init(1.0), // 1 second to expire
-     0,                        // machine 0
-     // no other conditions, always 'true'
-     (new Transition<MultiContext<dBFT2Context>>(machine0->getStateByName("Initial")))
-       ->add(Action<MultiContext<dBFT2Context>>(
-         "C := 0 | v := 0",
-         [](Timer& C, MultiContext<dBFT2Context>* d, int me) -> void {
-            cout << " => action: C := 0" << endl;
-            C.reset();
-            cout << " => action: v := 0" << endl;
-            d->vm[me].params->v = 0;
-         })));
-   */
-
-   // transition for PreInitial to Started
-   // preinitial -> started
-   preinitial->addTransition(
-     (new Transition<MultiContext<dBFT2Context>>(machine0->getStateByName("Started")))->add(Condition<MultiContext<dBFT2Context>>("OnStart", [](const Timer& t, MultiContext<dBFT2Context>* d, int me) -> bool {
-        cout << "Waiting for OnStart..." << endl;
-        return d->hasEvent("OnStart", me, vector<string>(0));
-     })));
-
-   // event scheduled to raise "OnStart" machine 0, after 1 seconds
-   multiMachine.scheduleEvent(
-     1.0, // 1 second to expire: after initialize()
-     0,   // machine 0
-     "OnStart",
-     vector<string>(0)); // no parameters
-
-   // event scheduled to raise "OnPrepareRequest" machine 0, after 3 seconds
-   multiMachine.scheduleEvent(
-     3.0, // 3 second to expire: after initialize()
-     0,   // machine 0
-     "OnPrepareRequest",
-     vector<string>(0)); // no parameters
-
-   MultiState<dBFT2Context> minitial(1, nullptr);
-   minitial[0] = machine0->getStateByName("PreInitial");
-
-   // run for 5.0 seconds max (watchdog limit)
-   multiMachine.setWatchdog(5.0);
-   multiMachine.run(&minitial, &ctx);
-}
-
-void
-dbft_test_primary()
-{
-   auto machine0 = create_dBFTMachine(0);
-
-   cout << "Machine => " << machine0->toString() << endl;
-
-   // v = 0, H = 1500, T = 3 (secs), R = 1 (one node network)
-   dBFT2Context data(0, 1500, 3, 1); // 1500 -> primary (R=1)
-
-   MultiContext<dBFT2Context> ctx;
-   ctx.vm.push_back(MachineContext<dBFT2Context>(&data, machine0));
-
-   // run for 5.0 seconds max
-   machine0->setWatchdog(5.0);
-   machine0->run(machine0->states[0], &ctx); // explicitly passing first state as default
 }
 
 void
