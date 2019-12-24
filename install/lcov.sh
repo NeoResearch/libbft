129f63e51325c23d7977508c022c939bb0648a42
Rodolfo Araujo <rodoufu@gmail.com>
2019-12-24 09:40:44 -0300

    All in the travis file.

--- install/lcov.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.14.orig.tar.gz
-tar xf lcov_1.14.orig.tar.gz
-sudo make -C lcov-1.14/ install
-gem install coveralls-lcov
--- install/vcpkg.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-git clone https://github.com/Microsoft/vcpkg.git
-cd vcpkg
-./bootstrap-vcpkg.sh
-./vcpkg integrate install
