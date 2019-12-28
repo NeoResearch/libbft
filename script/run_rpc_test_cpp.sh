b08d4c0f2a5411dab4bb923af6cae802a9c4f9a3
Rodolfo Araujo <rodoufu@gmail.com>
2019-12-28 11:00:33 -0300

    Moving scripts.

--- script/run_rpc_test_cpp.sh
+++ script/run_rpc_test_cpp.sh
@@ -5,6 +5,6 @@
 #./spec/cpp/build/app_RPCtest 2 4 1 S_drop_3_1000_500_Commit1 12
 
 
-./spec/cpp/build/app_RPCtest 0 4 1 S3_2000_500_Commit1 10  > /dev/null &
+./spec/cpp/build/app_RPCtest 0 4 1 S3_2000_500_Commit1 10 > /dev/null &
 ./spec/cpp/build/app_RPCtest 1 4 1 S3_2000_500_Commit1 11 > /dev/null &
 ./spec/cpp/build/app_RPCtest 2 4 1 S3_2000_500_Commit1 12
