bd1ce4835e170da60c24902b7887638ecda77d31
Rodolfo Araujo <rodoufu@gmail.com>
2019-12-28 18:49:59 -0300

    Action tests.

--- spec/cpp/tests/single/ActionTests.cpp
+++ spec/cpp/tests/single/ActionTests.cpp
@@ -47,3 +47,25 @@ TEST(SingleAction, UseTimedAction) {
       value = -1;
    }
 }
+
+struct ActionReference {
+   int age;
+};
+
+TEST(SingleAction, UseTimedActionReference) {
+   Action<ActionReference>::TimedActionType f = [](const Timer &t, ActionReference *p, const MachineId &id) -> void {
+      p->age = id.id;
+   };
+   unique_ptr<Action<ActionReference>> action(new Action<ActionReference>("T", f));
+   Clock clock;
+   Timer timer("T", &clock);
+
+   ActionReference p{-1};
+   const MachineId &id = MachineId(1);
+   f(timer, &p, id);
+   EXPECT_EQ(1, p.age);
+   p.age = -1;
+
+   action->timedAction(timer, &p, id);
+   EXPECT_EQ(1, p.age);
+}
