71f9528428764a8c6f110760c51ddbfb4eff97ed
Rodolfo Araujo <rodoufu@gmail.com>
2019-12-28 12:21:07 -0300

    Clean cache.

--- script/clean_cache.sh
+++ script/clean_cache.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
-rm -rf ${1}/grpc/third_party/protobuf/config.log
+rm -rf ${1}/grpc/third_party/protobuf/*.log
 rm -rf ${1}/grpc/third_party/protobuf/libtool
 rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/autom4te.cache/requests
-rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/config.log
+rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/*.log
 rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googlemock/autom4te.cache/requests
-rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googlemock/config.log
+rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googlemock/*.log
 rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googlemock/libtool
 rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googletest/autom4te.cache/requests
-rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googletest/config.log
+rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googletest/*.log
 #rm -rf ${1}/grpc/third_party/protobuf/third_party/
+rm -rf ${1}/grpc/libs/opt/*.a
+rm -rf ${1}/grpc/libs/opt/libgrpc++_error_details.so.1.27.0-dev
+rm -rf ${1}/grpc/libs/opt/libgrpcpp_channelz.so.1.27.0-dev
+rm -rf ${1}/grpc/libs/opt/libgrpc++_reflection.so.1.27.0-dev
+rm -rf ${1}/grpc/objs/opt/home/travis/build/NeoResearch/libbft/grpc/gens/src/proto/grpc/channelz/*.o
+rm -rf ${1}/grpc/objs/opt/home/travis/build/NeoResearch/libbft/grpc/gens/src/proto/grpc/core/*.o
+rm -rf ${1}/grpc/objs/opt/home/travis/build/NeoResearch/libbft/grpc/gens/src/proto/grpc/reflection/v1alpha/*.o
+#rm -rf ${1}/grpc/objs/opt/home/travis/build/NeoResearch/libbft/grpc/gens/
