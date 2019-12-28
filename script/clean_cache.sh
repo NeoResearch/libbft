6bd5abac14070b1d722f7c4c5249750e7ef7b073
Rodolfo Araujo <rodoufu@gmail.com>
2019-12-28 12:08:46 -0300

    Fix build cache.

---
+++ script/clean_cache.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+rm -rf ${1}/grpc/third_party/protobuf/config.log
+rm -rf ${1}/grpc/third_party/protobuf/libtool
+rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/autom4te.cache/requests
+rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/config.log
+rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googlemock/autom4te.cache/requests
+rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googlemock/config.log
+rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googlemock/libtool
+rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googletest/autom4te.cache/requests
+rm -rf ${1}/grpc/third_party/protobuf/third_party/googletest/googletest/config.log
+#rm -rf ${1}/grpc/third_party/protobuf/third_party/
