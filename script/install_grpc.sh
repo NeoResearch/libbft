243aed0af7cbd67ed25d3659d3c6b1bc52504447
Rodolfo Araujo <rodoufu@gmail.com>
2019-12-28 10:12:04 -0300

    It copies the folders but still needs to install the libs.

--- script/install_grpc.sh
+++ script/install_grpc.sh
@@ -3,8 +3,10 @@ echo "Checking folder ${1}grpc/"
 if test -f "${1}grpc/"; then
     echo "Installing gRPC"
     cd ${1}grpc/
-    make
     sudo make install
 else
-    echo "gRPC is already installed"
+    echo "Building and installing gRPC"
+    cd ${1}grpc/
+    make
+    sudo make install
 fi
--- script/install_protobuf.sh
+++ script/install_protobuf.sh
@@ -3,12 +3,15 @@ echo "Checking folder ${1}grpc/"
 if test -f "${1}grpc/"; then
     echo "Installing Protobuf"
     cd ${1}grpc/third_party/protobuf
+    sudo make install
+    sudo ldconfig
+else
+    echo "Building and installing Protobuf"
+    cd ${1}grpc/third_party/protobuf
     git submodule update --init --recursive
     sh autogen.sh
     sh configure --prefix=/usr
     make
     sudo make install
     sudo ldconfig
-else
-    echo "Protobuf is already installed"
 fi
