3a9d6d31ad4ea1aba4529938a9bd71fe6ca94911
Rodolfo Araujo <rodoufu@gmail.com>
2019-12-28 10:55:30 -0300

    Removing debug.

--- script/install_grpc.sh
+++ script/install_grpc.sh
@@ -2,19 +2,11 @@
 echo "Checking folder ${1}grpc/"
 if test -f "${1}grpc/"; then
     echo "Installing gRPC"
-    ls -lha
-    cd ${1}
-    ls -lha
-    cd grpc
-    ls -lha
+    cd ${1}grpc
     sudo make install
 else
     echo "Building and installing gRPC"
-    ls -lha
-    cd ${1}
-    ls -lha
-    cd grpc
-    ls -lha
+    cd ${1}grpc
     make
     sudo make install
 fi
--- script/install_protobuf.sh
+++ script/install_protobuf.sh
@@ -2,20 +2,12 @@
 echo "Checking folder ${1}grpc/"
 if test -f "${1}grpc/"; then
     echo "Installing Protobuf"
-    cd ${1}
-    ls -lha
-    cd grpc
-    ls -lha
-    cd third_party/protobuf
+    cd ${1}grpc/third_party/protobuf
     sudo make install
     sudo ldconfig
 else
     echo "Building and installing Protobuf"
-    cd ${1}
-    ls -lha
-    cd grpc
-    ls -lha
-    cd third_party/protobuf
+    cd ${1}/grpc/third_party/protobuf
     git submodule update --init --recursive
     sh autogen.sh
     sh configure --prefix=/usr
