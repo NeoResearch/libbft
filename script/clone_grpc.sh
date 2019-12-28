a8b9fb1e1b41838e074a32cbceee63722a4d7c4d
Rodolfo Araujo <rodoufu@gmail.com>
2019-12-28 10:35:25 -0300

    Fix clone grpc.

--- script/clone_grpc.sh
+++ script/clone_grpc.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo "Checking folder ${1}grpc/"
 if test -f "${1}grpc/"; then
+    echo "gRPC folder is already here"
+else
     echo "Cloning gRPC"
     cd ${1}
     git clone https://github.com/grpc/grpc
     cd grpc
     echo "Cloning Protobuf"
     git submodule update --init --recursive
-else
-    echo "gRPC folder is already here"
 fi
